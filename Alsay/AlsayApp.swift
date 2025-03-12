d9dc58a36826141d57b587bdbbac27fcb4f2290f
Al-say <2678448813@qq.com>
2025-03-12 18:46:24 +0800

    Initial Commit

---
+++ Alsay/AlsayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlsayApp.swift
+//  Alsay
+//
+//  Created by Say Al on 2025/3/12.
+//
+
+import SwiftUI
+
+@main
+struct AlsayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlsayTests/AlsayTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlsayTests.swift
+//  AlsayTests
+//
+//  Created by Say Al on 2025/3/12.
+//
+
+import Testing
+@testable import Alsay
+
+struct AlsayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
